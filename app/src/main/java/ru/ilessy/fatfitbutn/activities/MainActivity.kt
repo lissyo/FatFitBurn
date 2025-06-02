49ee402e998a448e1b6223a077b78fe959cf43eb
ilessy <igorekrussia@gmail.com>
2025-06-02 23:16:07 +0300

    Add immersive mode (temporary because this methods is deprecated)

--- app/src/main/java/ru/ilessy/fatfitbutn/activities/MainActivity.kt
+++ app/src/main/java/ru/ilessy/fatfitbutn/activities/MainActivity.kt
@@ -1,7 +1,11 @@
 package ru.ilessy.fatfitbutn.activities
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -44,11 +48,60 @@ class MainActivity : AppCompatActivity() {
                     supportFragmentManager.beginTransaction().replace(
                         R.id.fragment_container,
                         videoWorkoutFragment
-                    ).addToBackStack(null).commit()
+                    ).addToBackStack(null).commit().apply {
+                        goToImmersiveMode()
+                    }
                 }
             } ?: run {
                 //TODO добавить обработку
             }
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    @Suppress("DEPRECATION")
+    override fun onBackPressed() {
+        if (supportFragmentManager.findFragmentById(R.id.fragment_container) is VideoWorkoutFragment) {
+            exitImmersiveMode()
+        }
+        super.onBackPressed()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun goToImmersiveMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController.apply {
+                this?.hide(WindowInsets.Type.systemBars())
+                this?.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            val decorView = window.decorView
+            decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    )
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun exitImmersiveMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.show(WindowInsets.Type.systemBars())
+        } else {
+            val decorView = window.decorView
+            decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    )
         }
     }
 
