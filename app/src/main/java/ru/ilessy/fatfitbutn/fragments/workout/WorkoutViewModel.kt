17610e94c8de4f08028a798b9b404206975bccdb
ilessy <igorekrussia@gmail.com>
2025-06-02 20:06:00 +0300

    Add logic call to video endpoint get workout

--- app/src/main/java/ru/ilessy/fatfitbutn/fragments/workout/WorkoutViewModel.kt
+++ app/src/main/java/ru/ilessy/fatfitbutn/fragments/workout/WorkoutViewModel.kt
@@ -6,22 +6,36 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import ru.ilessy.domain.models.VideoWorkout
 import ru.ilessy.domain.models.Workout
+import ru.ilessy.domain.usecases.GetVideoUseCase
 import ru.ilessy.domain.usecases.GetWorkoutsUseCase
 import javax.inject.Inject
 
 @HiltViewModel
-class WorkoutViewModel @Inject constructor(private val getWorkoutsUseCase: GetWorkoutsUseCase) :
+class WorkoutViewModel @Inject constructor(
+    private val getWorkoutsUseCase: GetWorkoutsUseCase,
+    private val getVideoUseCase: GetVideoUseCase
+) :
     ViewModel() {
 
     private val _workoutsLiveData: MutableLiveData<List<Workout>> = MutableLiveData<List<Workout>>()
     val workoutsLiveData: LiveData<List<Workout>> = _workoutsLiveData
 
+    private val _videoLiveData: MutableLiveData<VideoWorkout> = MutableLiveData<VideoWorkout>()
+    val videoLiveData: LiveData<VideoWorkout> = _videoLiveData
+
     fun setIntent(workoutIntent: WorkoutIntent) {
         when (workoutIntent) {
-            WorkoutIntent.GetWorkouts -> {
+            is WorkoutIntent.GetWorkouts -> {
                 viewModelScope.launch {
                     _workoutsLiveData.value = getWorkoutsUseCase.invoke()
+                }
+            }
+
+            is WorkoutIntent.GetVideoWorkout -> {
+                viewModelScope.launch {
+                    _videoLiveData.value = getVideoUseCase.invoke(id = workoutIntent.workoutId)
                 }
             }
         }
@@ -31,4 +45,5 @@ class WorkoutViewModel @Inject constructor(private val getWorkoutsUseCase: GetWo
 
 sealed interface WorkoutIntent {
     data object GetWorkouts : WorkoutIntent
+    data class GetVideoWorkout(val workoutId: Long) : WorkoutIntent
 }
